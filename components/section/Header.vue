60efac1072ccdd2e371ec5a9a518999b366ec586
Luan Soares <luansesa@hotmail.com>
2025-01-27 17:46:42 -0300

    Fix header

--- components/section/Header.vue
+++ components/section/Header.vue
@@ -1,10 +1,10 @@
 <template>
 	<header class="p-4 bg-brand_blue text-white/80 shadow z-10">
-		<div class="container flex justify-between h-20 mx-auto">
-			<NuxtLink to="/" aria-label="Back to homepage" class="flex items-center justify-center overflow-hidden size-20 rounded-full bg-white">
-        <img src="/logo.jpg" alt="" class="size-14">
+		<div class="container flex justify-between h-14 md:h-20 mx-auto">
+			<NuxtLink to="/" aria-label="Back to homepage" class="flex items-center justify-center overflow-hidden size-14 md:size-20 rounded-full bg-white">
+        <img src="/logo.jpg" alt="" class="size-10 md:size-14">
       </NuxtLink>
-			<ul class="items-stretch hidden space-x-3 md:flex">
+			<ul class="flex items-stretch space-x-3">
 				<li v-for="(item, index) in items" :key="index" class="flex">
 					<NuxtLink
             :to="{ name: item.id }"
@@ -14,11 +14,11 @@
           >{{ item.label }}</NuxtLink>
 				</li>
 			</ul>
-			<button class="flex justify-end p-4 md:hidden">
+			<!-- <button class="flex justify-end p-4 md:hidden">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
 				</svg>
-			</button>
+			</button> -->
 		</div>
 	</header>
 </template>
