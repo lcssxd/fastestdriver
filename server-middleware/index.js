e390241c9893aa0fca967701ca81fbb251f55870
Luan Soares <luansesa@hotmail.com>
2025-01-28 01:13:47 -0300

    Updates

--- server-middleware/index.js
+++ server-middleware/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import sendEmail from '../api/send-email.js'; // Importa o handler da API
+import sendEmail from '../api/send-email'; // Importe o handler da API
 
 const app = express();
 
-// Middleware para lidar com JSON
+// Middleware para interpretar JSON no corpo da requisição
 app.use(express.json());
 
-// Endpoint para enviar e-mails
-app.post('/api/send-email', sendEmail);
+// Rota de API para enviar e-mails
+app.post('/send-email', sendEmail);
 
 export default app;
